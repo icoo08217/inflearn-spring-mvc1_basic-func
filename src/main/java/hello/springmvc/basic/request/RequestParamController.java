00242c162dc6e2e618133a783701e8dfc44ef3ce
icoo08217 <icoo08216@gmail.com>
2023-06-26 21:37:36 +0900

    HTTP 요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,9 +1,11 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -38,7 +40,7 @@ public class RequestParamController {
     /**
      * @RequestParam 사용
      * HTTP 파라미터 이름이 변수 이름과 같으면 @RequestParam(name="xx") 생략 가능
-     * */
+     */
     @ResponseBody
     @RequestMapping("/request-param-v3")
     public String requestParamV3(
@@ -53,7 +55,7 @@ public class RequestParamController {
     /**
      * @RequestParam 사용
      * String, int 등의 단순 타입이면 @RequestParam 도 생략 가능
-     * */
+     */
     @ResponseBody
     @RequestMapping("/request-param-v4")
     public String requestParamV4(String username, int age) {
@@ -64,14 +66,14 @@ public class RequestParamController {
     }
 
     /**
-     * @RequestParam.required
-     * /request-param-required -> username이 없으므로 예외 *
+     * @RequestParam.required /request-param-required -> username이 없으므로 예외 *
      * 주의!
      * /request-param-required?username= -> 빈문자로 통과 *
      * 주의!
      * /request-param-required
      * int age -> null을 int에 입력하는 것은 불가능, 따라서 Integer 변경해야 함(또는 다음에 나오는
-    defaultValue 사용) */
+     * defaultValue 사용)
+     */
     @ResponseBody
     @RequestMapping("/request-param-required")
     public String requestParamRequired(
@@ -83,8 +85,7 @@ public class RequestParamController {
     }
 
     /**
-     * @RequestParam
-     * - defaultValue 사용 *
+     * @RequestParam - defaultValue 사용 *
      * 참고: defaultValue는 빈 문자의 경우에도 적용 * /request-param-default?username=
      */
     @ResponseBody
@@ -104,9 +105,29 @@ public class RequestParamController {
      */
     @ResponseBody
     @RequestMapping("/request-param-map")
-    public String requestParamMap(@RequestParam Map<String, Object> paramMap){
+    public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
 
         log.info("username = {} , age = {}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    /**
+    * @ModelAttribute 사용
+    * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때 자세히 설명
+    * */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username={} , age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={} , age={}", helloData.getUsername(), helloData.getAge());
+
         return "ok";
     }
 }
